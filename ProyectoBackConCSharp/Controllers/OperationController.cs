901ec2580ca82ebc44198acd36b91b9124086f42
CrismarJC <7612@itescam.edu.mx>
2026-01-22 09:56:12 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoBackConCSharp/Controllers/OperationController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProyectoBackConCSharp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OperationController : ControllerBase
+    {
+        [HttpGet]
+        public decimal Get(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        [HttpPost]
+        public decimal Add(int a, int b)
+        {
+            return a - b;
+        }
+
+        [HttpPut]
+        public decimal Edit(int a, int b)
+        {
+            return a * b;
+        }
+
+
+        [HttpDelete]
+        public decimal Delete(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
