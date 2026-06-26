86832b797fce9cda80d5154d10692d3324df5921
CrismarJC <7612@itescam.edu.mx>
2026-06-26 16:11:05 -0600

    Se agregan mensajes de error personalizados para el BeerInsertValidator

--- ProyectoBackConCSharp/Validators/BeerInsertValidator.cs
+++ ProyectoBackConCSharp/Validators/BeerInsertValidator.cs
@@ -7,7 +7,10 @@ namespace ProyectoBackConCSharp.Validators
     {
         public BeerInsertValidator() { 
         
-            RuleFor (x => x.Name).NotEmpty();
+            RuleFor (x => x.Name).NotEmpty().WithMessage("El nombre es obligatorio");
+            RuleFor(x => x.Name).Length(2,20).WithMessage("El nombre debe medir de 2 a 20 caracteres");
+            RuleFor(x => x.BrandID).GreaterThan(0).WithMessage("Error con el valor enviado de marca");
+            RuleFor(x => x.Alcohol).GreaterThan(0).WithMessage("El {PropertyName} debe ser mayor a 0");
         }
     }
 }
